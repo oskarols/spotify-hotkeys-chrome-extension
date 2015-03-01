8e50cea523b02918a58597adfe964ff14ff7296c
Oskar Olsson <oskar.ols@gmail.com>
2015-03-01 17:48:59 +0100

    Restructured to allow for actions upon different panels
    - Added command to add the currently playing song to "My Music"
      to allow user to save song without looking at player
      (i.e. great for radio).

--- src/background.js
+++ src/background.js
@@ -1,3 +1,36 @@
+console.log('INIT');
+
+function getCommandData (command) {
+  function undefinedDocument () {
+    console.log('SPOTIFY::UNDEFINED PANEL COMMAND::' + command);
+    return undefined;
+  };
+
+  var playerPanelID = '#app-player',
+      collectionPanelID = '[id^="collection-app-spotify"]'; // partial, match first since:
+  // Collection ID example: "collection-app-spotify:app:collection-0-1425223339731"
+
+  // If missing selector, assume key is selector
+  var commandMap = {
+    'play-pause': {
+      panelID: playerPanelID
+    },
+    'next': {
+      panelID: playerPanelID
+    },
+    'previous': {
+      panelID: playerPanelID
+    },
+    'track-add': {
+      panelID: collectionPanelID,
+      selector: 'tr.current.playing .btn-add' // need to use tr else it confuses it with something else ..
+    },
+    'delete-from-playlist': '' // not sure what to od here ..
+  };
+
+  return commandMap[command] || undefinedDocument();
+};
+
 function onCommand(command) {
   chrome.tabs.query({url: 'https://play.spotify.com/*'}, function(tabs) {
 
@@ -6,14 +39,20 @@ function onCommand(command) {
       chrome.tabs.create({url: 'https://play.spotify.com'});
     }
 
-    var code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+    var commandData = getCommandData(command),
+        commandActionSelector = commandData.selector || '#' + command;
+
+    // spotify UI consists of several iframes (i.e. 'documents')
+    var code = "document.querySelector('" + commandData.panelID + "').contentDocument.querySelector('" + commandActionSelector + "').click()";
+
+    console.log(code);
     // Apply command on all spotify tabs.
     for (var tab of tabs) {
       chrome.tabs.executeScript(tab.id, {code: code});
     }
 
-    // Unload background page as soon as we're done.
-    window.close();
+    // Unload background page as soon as we're done. Disable this for debugging
+    // window.close();
   });
 };
 
