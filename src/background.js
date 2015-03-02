aa10dcc2e3311ea54f0097349bfdf8858808f718
Oskar Olsson <oskar.ols@gmail.com>
2015-03-02 16:42:55 +0100

    Cleanup of background script

--- src/background.js
+++ src/background.js
@@ -1,5 +1,4 @@
 console.log('INIT SPOTIFY EXTENSION');
-// chrome.runtime.sendMessage(string extensionId, any message, object options, function responseCallback)
 
 var port = chrome.runtime.connect();
 
@@ -15,17 +14,3 @@ function onCommand(command) {
 }
 
 chrome.commands.onCommand.addListener(onCommand);
-
-
-// chrome.runtime.onMessage.addListener(function callback)
-
-// window.addEventListener("spotify.enhancement.", function(event) {
-//   // We only accept messages from ourselves
-//   if (event.source != window)
-//     return;
-
-//   if (event.data.type && (event.data.type == "FROM_PAGE")) {
-//     console.log("Content script received: " + event.data.text);
-//     port.postMessage(event.data.text);
-//   }
-// }, false);
